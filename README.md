49949c2cd3743357df5f909048a8da2641a54938
Baran Önen <baranonenn@gmail.com>
2021-04-22 00:54:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cyberflight.github.io
+ 
---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+function logoCheck() {
+    if (window.innerWidth > 1200) {
+        if (window.scrollY > 230 && window.scrollY < 340) {
+            document.getElementById("SSALLogo").style.display = "none"
+            document.getElementById("CyberFlightLogo").style.display = "block"
+            document.getElementById("CyberFlightLogo").style.transform = "rotateY(" + scale(window.scrollY, 230, 340, 0, 90) + "deg)"
+        } else if (window.scrollY > 340 && window.scrollY < 450) {
+            document.getElementById("CyberFlightLogo").style.display = "none"
+            document.getElementById("SSALLogo").style.display = "block"
+            document.getElementById("SSALLogo").style.transform = "rotateY(" + scale(window.scrollY, 340, 450, 90, 0) + "deg)"
+        } else if (window.scrollY < 340) {
+            document.getElementById("SSALLogo").style.display = "none"
+            document.getElementById("CyberFlightLogo").style.display = "block"
+            document.getElementById("CyberFlightLogo").style.transform = "rotateY(0deg)"
+        } else if (window.scrollY > 450) {
+            document.getElementById("SSALLogo").style.display = "block"
+            document.getElementById("CyberFlightLogo").style.display = "none"
+            document.getElementById("SSALLogo").style.transform = "rotateY(0deg)"
+        }
+    } else {
+        document.getElementById("CyberFlightLogo").style.display = "block"
+        document.getElementById("SSALLogo").style.display = "none"
+        document.getElementById("CyberFlightLogo").style.transform = "rotateY(0deg)"
+    }
+}
+
+function arCheck() {
+    if (document.getElementById("logocontainer").relList.supports("ar")) {
+        document.getElementById("logocontainer").rel = "ar"
+        document.getElementById("logocontainer").href = "./assets/CyberFlightLogo.usdz"
+    }
+}
